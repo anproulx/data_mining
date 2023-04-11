9ee309c2d81f04ceea6613d3b016d36bfd80411b
gijssmeets <gijsolinsmeets@gmail.com>
2023-04-11 15:42:56 +0200

    new code!! now aggregated data in less lines

---
+++ code_1.py
@@ -0,0 +1,54 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+print(os.getcwd())
+
+data = pd.read_csv('dataset_mood_smartphone.csv')
+
+del data['Unnamed: 0']
+
+for col in data['variable'].unique():
+    newcol = data[data['variable'] == col]
+    newcol = newcol.rename(columns={'value': col})
+    data[col] = newcol[col]
+
+
+
+d = {}
+ids = data['id'].unique()
+for id in ids:
+    data_per_id = data.loc[(data['id'] == id)]
+
+    ## Set timestamp to 24 hour
+    data_per_id_time = data_per_id.set_index(['time'])
+    data_per_id_time.index = pd.to_datetime(data_per_id_time.index)
+
+    # Get the values for each variable
+    # Mean of in range values
+    new_data = pd.DataFrame()
+    set_time = False
+    for col in data['variable'].unique():
+
+        if col in ['mood', 'circumplex.arousal', 'circumplex.valence']:
+            col_agg = data_per_id_time.resample('24H').agg({col: 'mean'})
+        else:
+            col_agg = data_per_id_time.resample('24H').agg({col: 'sum'})
+
+        if set_time == False:
+            new_data['id'] = [id]*len(col_agg.index.values.flatten())
+            new_data['time'] = col_agg.index
+
+            set_time = True
+
+        new_data[col] = col_agg.values.flatten()
+
+
+    d["user{0}".format(id)] = new_data
+
+for id in d:
+    print(id)
+    
+print(d['userAS14.01'])
